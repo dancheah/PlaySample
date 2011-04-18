ab6fbf57da00c3198cc2e3a2fb3ddb5710fdda14
Daniel Cheah <me@dancheah.com>
2011-04-18 14:36:55 -0700

    Worked through chapter on custom editor

--- yabe-tutorial/app/controllers/Admin.java
+++ yabe-tutorial/app/controllers/Admin.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import models.Post;
+import models.Tag;
 import models.User;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
+
+import java.util.List;
 
 @With(Secure.class)
 public class Admin extends Controller {
@@ -16,6 +20,45 @@ public class Admin extends Controller {
     }
 
     public static void index() {
+        List<Post> posts = Post.find("author.email", Security.connected()).fetch();
+        render(posts);
+    }
+
+    public static void form(Long id) {
+        if(id != null) {
+            Post post = Post.findById(id);
+            render(post);
+        }
         render();
+    }
+
+    public static void save(Long id, String title, String content, String tags) {
+        Post post;
+        if(id == null) {
+            // Create post
+            User author = User.find("byEmail", Security.connected()).first();
+            post = new Post(author, title, content);
+        } else {
+            // Retrieve post
+            post = Post.findById(id);
+            // Edit
+            post.title = title;
+            post.content = content;
+            post.tags.clear();
+        }
+        // Set tags list
+        for(String tag : tags.split("\\s+")) {
+            if(tag.trim().length() > 0) {
+                post.tags.add(Tag.findOrCreateByName(tag));
+            }
+        }
+        // Validate
+        validation.valid(post);
+        if(validation.hasErrors()) {
+            render("@form", post);
+        }
+        // Save
+        post.save();
+        index();
     }
 }
